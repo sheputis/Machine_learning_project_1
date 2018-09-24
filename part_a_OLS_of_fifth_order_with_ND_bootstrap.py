3551bb761fd434a07a886d153f85aee6bb38b56e
sheputis <arnoldas@student.matnat.uio.no>
2018-09-24 18:55:01 +0200

    finished the bootstrap class

--- part_a_OLS_of_fifth_order_with_ND_bootstrap.py
+++ part_a_OLS_of_fifth_order_with_ND_bootstrap.py
@@ -13,76 +13,16 @@ def FrankeFunction(x,y,noise=0):
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4 + noise
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
-# Make data.
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05) #0.05
-n = y.size
-
-one_vec = x*0+1
-zigma = 0.1 #this is the variance of the noise var(y)
-noise = zigma*np.random.randn(n,n)
-
-f=FrankeFunction(x,y,noise)
-
-#beta_lin_reg = (np.linalg.inv(X.T.dot(X)).dot(X.T)).dot(f)
-x, y = np.meshgrid(x,y) #the x's and y's are the matrices that will be plotted
-x_, y_,noise_ = x.reshape(-1, 1), y.reshape(-1,1), noise.reshape(-1,1) #the x_'s and y_'s will be the vectors used for calculation
-
-X_ = np.c_[x_,y_]
-
-poly = PolynomialFeatures(5)
-X_ = poly.fit_transform(X_) #generating the sample matrix with two variables that are both polynomials of 5th order
-
-
-
-
-z = FrankeFunction(x, y,noise)
-z_= FrankeFunction(x_, y_,noise_)
-beta_lin_reg = (np.linalg.inv(X_.T.dot(X_)).dot(X_.T)).dot(z_)
-
-
-z_fit_ = X_.dot(beta_lin_reg)
-z_fit=z_fit_.reshape((20,20))
-
-print("_____________________________calculating variance in beta variables________________________________________")
-var_Beta = (np.linalg.inv(X_.T.dot(X_)))*zigma
-for i in range(21): #writing out variances
-    print(var_Beta[i][i])
-
-print("____________________________________________errors_________________________________________________________")
-print("Mean squared error: %.5f" % mean_squared_error(z_,z_fit_))
-print("R2r2_score: %.5f" % r2_score(z_,z_fit_))
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-surf2 = ax.plot_surface(x,y,z_fit, cmap=cm.coolwarm,
-                      linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
-#plt.show()
 
 print("____________________________________________BOOTSTRAP________________________________________________________")
 
 class bootstrap:
-    def __init__(self,original_array_x,original_array_y):
-        self.original_array_x = original_array_x
-        self.original_array_y = original_array_y
-        self.n = len(original_array_x)
+    def __init__(self,n): #n the length of the array that is put in
+        self.n = n
         self.n_test = None;
         self.array_of_indices_training = self.generate_training_indices()
         self.array_of_indices_test = self.generate_test_indices()
-        self.training_data_x , self.training_data_y = self.generate_training_data()
-        self.test_data_x , self.test_data_y = self.generate_test_data()
 
     def generate_training_indices(self):
         return np.random.random_integers(self.n,size = self.n)-1 #indices from 0 to n-1
@@ -95,35 +35,128 @@ class bootstrap:
         test_indices = np.array(test_indices)
         return test_indices
 
-    def generate_training_data(self):
-        temp_x = self.original_array_x.copy()
-        temp_y = self.original_array_y.copy()
+    def generate_training_data(self,input_array):
+        temp = input_array.copy()
         for i in range(self.n):
-            temp_x[i] = self.original_array_x[self.array_of_indices_training[i]]
-            temp_y[i] = self.original_array_y[self.array_of_indices_training[i]]
-        return temp_x , temp_y
-    def generate_test_data(self):
+            temp[i] = input_array[self.array_of_indices_training[i]]
+        return temp
+    def generate_test_data(self,input_array):
         self.n_test = len(self.array_of_indices_test)
-        temp_x = self.original_array_x[:self.n_test].copy()
-        temp_y = self.original_array_x[:self.n_test].copy()
+        temp = input_array[:self.n_test].copy()
         for i in range(self.n_test):
-            temp_x[i] = self.original_array_x[self.array_of_indices_test[i]]
-            temp_y[i] = self.original_array_y[self.array_of_indices_test[i]]
-        return temp_x,temp_y
-
+            temp[i] = input_array[self.array_of_indices_test[i]]
+        return temp
+"""
 a=np.array([[1],[2],[3],[4],[5]])
-b=np.array([[10],[20],[30],[40],[50]])
 print("c.original_array_x")
 print(a)
-c = bootstrap(a,b)
-c.generate_training_indices()
-print("c.array_of_indices_training")
-print(c.array_of_indices_training)
-print("c.array_of_indices_test")
-print(c.array_of_indices_test)
-print("c.training_data_x")
-print(c.training_data_x)
-print("c.training_data_y")
-print(c.training_data_y)
-print("c.test_data_x")
-print(c.test_data_x)
+print('_________')
+c = bootstrap(a)
+print(c.training_data)
+print('_________')
+print(c.test_data)
+print('_________')
+"""
+class OLS_main:
+    def __init__(self):
+        delta=0.05
+        x = np.arange(0, 1, delta)
+        y = np.arange(0, 1, delta) #0.05
+        n = len(x)
+        self.x, self.y = np.meshgrid(x,y) #the x's and y's are the matrices that will be plotted
+        self.n = self.y.size
+        self.zigma = 0.1 #this is the variance of the noise var(y)
+        self.noise = self.zigma*np.random.randn(n,n)
+        self.x_, self.y_,self.noise_ = self.x.reshape(-1, 1), self.y.reshape(-1,1), self.noise.reshape(-1,1) #the x_'s and y_'s will be the vectors used for calculation
+        self.X_ = self.generate_X_of_degree(5)
+        self.z  = FrankeFunction(self.x, self.y,self.noise)
+        self.z_ = FrankeFunction(self.x_, self.y_,self.noise_)
+        self.beta_lin_reg = (np.linalg.inv(self.X_.T.dot(self.X_)).dot(self.X_.T)).dot(self.z_)
+        self.z_fit_ = self.X_.dot(self.beta_lin_reg)
+        self.z_fit=self.z_fit_.reshape((n,n))
+
+
+    def generate_X_of_degree(self,n):
+        X_ = np.c_[self.x_,self.y_]
+        poly = PolynomialFeatures(n)
+        return poly.fit_transform(X_) #generating the sample matrix with two variables that are both polynomials of 5th order
+
+    def plot_everything(self):
+        print("________________________________________plotting_________________________________________________________")
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(self.x, self.y, self.z, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+        surf2 = ax.plot_surface(self.x,self.y,self.z_fit, cmap=cm.coolwarm,
+                              linewidth=0, antialiased=False)
+        # Customize the z axis.
+        ax.set_zlim(-0.10, 1.40)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        # Add a color bar which maps values to colors.
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+
+    def variance_in_beta(self):#this needs to be edited, the number 21 has to be changed to the amount of columns in X (polyfit)
+        print("_____________________________calculating variance in beta variables________________________________________")
+        var_Beta = (np.linalg.inv(self.X_.T.dot(self.X_)))*self.zigma
+        for i in range(21): #writing out variances
+            print(var_Beta[i][i])
+
+    def errors(self):
+        print("____________________________________________errors_________________________________________________________")
+        mse_ = mean_squared_error(self.z_,self.z_fit_)
+        r2_score_ = r2_score(self.z_,self.z_fit_)
+        print("Mean squared error: %.5f" % mse_)
+        print("R2r2_score: %.5f" % r2_score_)
+        return mse_ , r2_score_
+
+
+class OLS_:
+    def __init__(self,x,y,z): #x,y are the coordinates and z is the corresponding precalculated(with noise) function output
+        self.x = x
+        self.y = y
+        self.z = z
+        self.X = self.generate_X_of_degree(5)
+        self.beta = self.find_beta()
+        self.z_fit = self.X.dot(self.beta)
+
+    def generate_X_of_degree(self,n):
+        X = np.c_[self.x,self.y]
+        poly = PolynomialFeatures(n)
+        return poly.fit_transform(X) #generating the sample matrix with two variables that are both polynomials of 5th order
+
+    def find_beta(self):
+        return (np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T)).dot(self.z)
+
+    def errors(self):
+        print("____________________________________________errors_________________________________________________________")
+        mse_ = mean_squared_error(self.z,self.z_fit)
+        r2_score_ = r2_score(self.z,self.z_fit)
+        print("Mean squared error bootstrap: %.5f" % mse_)
+        print("R2r2_score bootstrap: %.5f" % r2_score_)
+        return mse_ , r2_score_
+print("))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))")
+
+def errors(z,z_fit):
+    print("____________________________________________errors_________________________________________________________")
+    mse_ = mean_squared_error(z,z_fit)
+    r2_score_ = r2_score(z,z_fit)
+    print("Mean squared error: %.5f" % mse_)
+    print("R2r2_score: %.5f" % r2_score_)
+    return mse_ , r2_score_
+
+A = OLS_main()
+A.errors()
+BOOT = bootstrap(len(A.x_))
+x_train = BOOT.generate_training_data(A.x_)
+y_train = BOOT.generate_training_data(A.y_)
+z_train = BOOT.generate_training_data(A.z_)
+
+x_test = BOOT.generate_test_data(A.x_)
+y_test = BOOT.generate_test_data(A.y_)
+z_test = BOOT.generate_test_data(A.z_)
+
+
+B = OLS_(x_train,y_train,z_train)
+B.errors()
